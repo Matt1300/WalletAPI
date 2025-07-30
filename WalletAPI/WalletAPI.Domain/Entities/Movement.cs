e4e99dc464f8f9f5e8845b6434a7e9a37d9f4565
Matt1300 <mateogranizo1304@gmail.com>
2025-07-29 21:11:54 -0500

    Agregando entidades princiaples en la capa Dominio

---
+++ WalletAPI/WalletAPI.Domain/Entities/Movement.cs
@@ -0,0 +1,29 @@
+﻿namespace WalletAPI.Domain.Entities;
+public enum MovementType
+{
+    Credit,
+    Debit
+}
+public class Movement
+{
+    public int Id { get; private set; }
+    public Guid WalletId { get; private set; }
+    public decimal Amount { get; private set; }
+    public MovementType Type { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Wallet Wallet { get; private set; } 
+
+    private Movement() { }
+
+    public Movement(Guid walletId, decimal amount, MovementType type)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("El monto debe ser positivo.", nameof(amount));
+
+        WalletId = walletId;
+        Amount = amount;
+        Type = type;
+        CreatedAt = DateTime.UtcNow;
+    }
+}
